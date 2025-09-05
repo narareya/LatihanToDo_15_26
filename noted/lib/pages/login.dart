91e9af26e56ca07624386853f555b8a63c100072
narareya <nomnomhiranami@gmail.com>
2025-09-05 17:25:54 +0700

    feat [EJL] login page

---
+++ noted/lib/pages/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:noted/components/custom_button.dart';
+import 'package:noted/components/custom_textfield.dart';
+import 'package:noted/controller/Auth_controller.dart';
+
+class loginPage extends StatelessWidget {
+  loginPage({super.key});
+  final authController = Get.find<AuthController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomTextField(controller: authController.username, label: "Username"),
+            CustomTextField(controller: authController.password, label: "Password", obscure: true,),
+            SizedBox(height: 20,),
+            CustomButton(myText: "Login", onPressed: () {
+              authController.login();
+            })
+          ],
+        ),
+      )
+    );
+  }
+}
