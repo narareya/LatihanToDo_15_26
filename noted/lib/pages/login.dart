55bc5b72e9bd224f83ddf986bdb586c52e1ece91
nmdntaa <144686774+nmdntaa@users.noreply.github.com>
2025-10-12 14:55:19 +0700

    fix [NM] navbar error

--- noted/lib/pages/login.dart
+++ noted/lib/pages/login.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:noted/controller/Auth_controller.dart';
 import 'package:noted/controller/Widget_controller.dart';
 import 'package:noted/pages/login_widgets/login_mobile.dart';
 import 'package:noted/pages/login_widgets/login_wide.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
-
-  final authController = Get.find<AuthController>();
-  final controller = Get.find<WidgetController>();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints){
-          controller.updateLayout(constraints);
-          return Obx(()=> controller.isMobile.value ? LoginMobile() : LoginWide() );
-        }
-        )
+    final widgetController = Get.find<WidgetController>();
+    
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        widgetController.updateLayout(context);
+        
+        return Obx(() => widgetController.isWideScreen.value 
+          ? LoginWide() 
+          : LoginMobile()
+        );
+      },
     );
   }
 }
